c6cc5ea6991234d9c848130b76897f932687d986
Carlos A Afanador <carlosafanador97@gmail.com>
2024-06-11 15:55:10 -0500

    remove dependancy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
 
   useEffect(() => {
     fetchFields();
-  }, [fetchFields]);
+  }, []);
 
   const handleSearch = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
